d41a1aa720cde6bc8454bde66d73ad9fa522059f
Sid Navali <sn446@cornell.edu>
2018-12-04 22:18:17 -0500

    Pushing LSTM updates

--- train_lstm.py
+++ train_lstm.py
@@ -6,6 +6,9 @@ import keras.preprocessing.text as kt
 from keras.backend.tensorflow_backend import set_session
 from keras.layers import Embedding, LSTM, Dropout, TimeDistributed, Activation, Input, Dense, Flatten
 from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint
+
+checkpointer = ModelCheckpoint(filepath='trump_rev.hdf5', verbose=1, save_best_only=True)
 
 # Prevent allocation of all gpu-memory
 config = tf.ConfigProto()
@@ -68,7 +71,8 @@ for i in words.keys():
     idx2word[counter] = i        
         
 vocab_size = len(word2idx.keys())
-
+print("THIS IS THE VOCAB SIZE")
+print(vocab_size)
 
 # Creating training dataset with correct integer values
 x_data = np.zeros([train_df.shape[0], max_length + 1])
@@ -92,12 +96,24 @@ model.add(Embedding(vocab_size, embedding_vecor_length, input_length=32))
 model.add(LSTM(100, return_sequences = True))
 model.add(LSTM(100))
 model.add(Dense(100, activation='relu'))
-model.add(Dense(100, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
+# Shuffle and create a validation dataset
+# indices = n.random.permutation(1089)
+# index = 870
 
-model.fit(x_data, y_data, batch_size=16, epochs=25, shuffle=True, verbose = 1)
+# train_indices, valid_indices = indices[:index], indices[index:]
+# train_data, valid_data = x_data[train_indices], x_data[valid_indices]
 
-model.save("trump.h5")
+# y_data = np.clip(y_data, 0, 1)
+# train_labels, valid_labels = y_data[train_indices], y_data[valid_data]
+
+x_reverse_data = np.zeros([1089,32])
+for i in range(1089):
+    for j in range(32):
+        x_reverse_data[i, 31 - j] = x_data[i,j]
+
+model.fit(x_data, np.clip(y_data, 0, 1), batch_size=16, epochs=25, shuffle=True, verbose = 1, validation_split = 0.2, callbacks = [checkpointer] )
+
